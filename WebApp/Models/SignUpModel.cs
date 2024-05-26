f52b196e960a7646aee01c4f6c71690fe197657f
Clas <arvisclas@gmail.com>
2024-05-26 17:03:45 +0200

    Validation is updated

--- WebApp/Models/SignUpModel.cs
+++ WebApp/Models/SignUpModel.cs
@@ -6,26 +6,29 @@ namespace WebApp.Models;
 public class SignUpModel
 {
     [Required]
+    [DataType(DataType.Text)]
     [Display(Name = "First name", Prompt = "Enter your first name")]
     public string FirstName { get; set; } = null!;
 
     [Required]
+    [DataType(DataType.Text)]
     [Display(Name = "Last name", Prompt = "Enter your last name")]
     public string LastName { get; set; } = null!;
 
     [Required]
-    [Display(Name = "E-mail adress", Prompt = "Enter your e-mail adress")]
     [DataType(DataType.EmailAddress)]
+    [Display(Name = "E-mail adress", Prompt = "Enter your e-mail adress")]
     public string Email { get; set; } = null!;
 
     [Required]
-    [Display(Name = "Password", Prompt = "Enter your password")]
     [DataType(DataType.Password)]
+    [Display(Name = "Password", Prompt = "Enter your password")]
+    
     public string Password { get; set; } = null!;
 
     [Required]
-    [Display(Name = "Confirm Password", Prompt = "You have to confirm your password")]
     [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password", Prompt = "You have to confirm your password")]
     [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
     public string ConfirmPassword { get; set; } = null!;
 
