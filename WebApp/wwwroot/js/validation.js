8cb95a483df8b6c06615632f80801a399dde8fcc
Clas <arvisclas@gmail.com>
2024-05-27 17:28:24 +0200

    Added javascript

--- WebApp/wwwroot/js/validation.js
+++ WebApp/wwwroot/js/validation.js
@@ -18,13 +18,6 @@ const formErrorHandler = (element, validationResult) => {
 }
 
 
-const lengthValidator = (element, minLenght = 2) => {
-    if (element.value.lenght >= minLenght)
-        return true 
-
-    return false
-}
-
 const compareValidator = (element, compareValue) => {
     if (element.value === compareValue)
         return true
@@ -32,19 +25,69 @@ const compareValidator = (element, compareValue) => {
     return false    
 }
 
-const checkedValidator = (element) => {
-    if (element.checked)
-        return true 
 
-    return false
+const textValidation = (element, minLenght = 2) => {
+    if (element.value.lenght >= minLenght)
+        formErrorHandler(element, true)
+
+    formErrorHandler(element, false)
 }
 
+const emailValidator = (element) => {
+    const regEx = /^[a-zA-Z0–9._-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,4}$/
+    formErrorHandler(element, regEx.test(element.value))
+}
 
-
-const textValidation = (element) => {
-    formErrorHandler(element, lengthValidator(element))
+const passwordValidator = (element) => {
+    if (element.dataset.valEqualtoOther != undefined) {
+        formErrorHandler(element, compareValidator(element.value, document.getElementsByName(element.dataset.valEqualtoOther.replace('*','Form')[0].value)))
+    }
+    else {
+        const regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!^%*?&]{8,15}$/
+        formErrorHandler(element, regEx.test(element.value))
+    }   
 }
 
 const checkboxValidator = (element) => {
-    formErrorHandler(element, checkedValidator(element))
+    if (element.checked)
+        formErrorHandler(element, true)
+
+    formErrorHandler(element, false)
+
 }
+
+let forms = document.querySelectorAll('form')
+let inputs = forms[0].querySelectorAll('input')
+
+inputs.forEach(input => {
+    if (input.dataset.val === 'true') {
+
+        if (input.type === 'checkbox') {
+            input.addEventListener('change', (e) => {
+                checkboxValidator(e.target)
+            })
+        }
+        else {
+
+            input.addEventListener('keyup', (e) => {
+
+                switch (e.target.type) {
+
+                    case 'text':
+                        textValidation(e.target)
+                        break;
+
+                    case 'email':
+                        emailValidator(e.target)
+                        break;
+
+                    case 'password':
+                        passwordValidator(e.target)
+                        break;
+
+                }
+
+            })
+        }
+    }
+ }) 
